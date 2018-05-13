38149c906bbab21949df7249ff5ad7271909f5ca
NilMV <nil.medvedev17@gmail.com>
2018-05-13 13:50:24 +0300

    fix InsertionSort and create MergeSort algo

--- algorithms/insertion-sort/insertion-sort.go
+++ algorithms/insertion-sort/insertion-sort.go
@@ -1,24 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 //InsertionSort algorithm based on Cormen's book
-func InsertionSort(s []int) {
+func InsertionSort(s []int) []int {
 	for j := 1; j < len(s); j++ {
 		key := s[j]
 		i := j - 1
-		for i > 0 && s[i] > key {
+		for i >= 0 && s[i] > key {
 			s[i+1] = s[i]
 			i = i - 1
 		}
 		s[i+1] = key
 	}
-	fmt.Printf("sorted: s is %v\n", s)
+	return s
 
 }
 
 func main() {
-	s := []int{1, 2, 66, 66, 3, 5, 999, 324}
-	fmt.Printf("Befor sorting: s is %v\n", s)
-	InsertionSort(s)
+
+	slice := generateSlice(20)
+	fmt.Println("\n--- Unsorted --- \n\n", slice)
+	fmt.Println("\n--- Sorted ---\n\n", InsertionSort(slice), "\n")
+}
+
+// Generates a slice of size, size filled with random numbers
+func generateSlice(size int) []int {
+
+	slice := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(999) - rand.Intn(999)
+	}
+	return slice
 }
